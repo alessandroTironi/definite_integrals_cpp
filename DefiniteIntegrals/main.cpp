965bb408ad2010548199e762b4ce71c4286c7b69
Alessandro Tironi <alessandro.tironi7@gmail.com>
2019-05-15 08:33:34 +0200

    Selected best parameters for benchmarks.

--- DefiniteIntegrals/main.cpp
+++ DefiniteIntegrals/main.cpp
@@ -10,7 +10,7 @@
 // Precision of the integration (number of intervals)
 #define PRECISION 10000
 // Instances to compute for each function.
-#define ITERATIONS 1000
+#define ITERATIONS 10000
 
 // Macros for efficient testing code.
 #define GET_TIMESTAMP(timestamp_name)	time_stamp timestamp_name = time_point_cast<microseconds>(system_clock::now())
@@ -33,24 +33,28 @@ using time_stamp = std::chrono::time_point<std::chrono::system_clock,
 }
 
 
-inline real_t parabola(real_t x)
+real_t parabola(real_t x)
 {
 	return 2 * x* x + 3 * x + 12;
 }
 
-inline real_t gauss(real_t x)
+real_t gauss(real_t x)
 {
 	// Fixed mean and stdev, for test purposes
-	real_t mean = 10.f;
-	real_t stdev = 1.5f;
+	real_t mean = 0.0f;
+	real_t stdev = 1.0f;
 	real_t e = (x - mean) / stdev;
 	return exp(-0.5f * e * e) * 1 / (stdev * sqrt(2 * 3.14f));
 }
 
 int main()
 {
-	std::cout << "Starting parabola tests..." << std::endl;
+	std::cout << "Starting performance tests..." << std::endl
+		<< "Precision:\t\t\t" << PRECISION << std::endl
+		<< "Iterations for each test:\t" << ITERATIONS << std::endl << std::endl;
 
+	/*
+	std::cout << "Starting parabola tests..." << std::endl;
 	// Integration of simple parabola with rectangles approximation
 	TEST_PERFORMANCE(definite_integral_rectangles(parabola, 0.0f, 1.0f, PRECISION), t1_rect,
 		"int(parabola) with rectangles approximation");
@@ -66,29 +70,30 @@ int main()
 	// Integration of simple parabola with Cavalieri-Simpson approximation + SSE
 	TEST_PERFORMANCE(definite_integral_cs_sse(parabola, 0.0f, 1.0f, PRECISION), t1_cs_sse,
 		"int(parabola) with Cavalieri-Simpson rule approximation + SSE");
+	*/
 
 	std::cout << "Starting Gaussian function tests..." << std::endl;
 	// Gaussian integration with rectangle approximation
-	TEST_PERFORMANCE(definite_integral_rectangles(gauss, 0.0f, 20.0f, PRECISION), t2_rect,
+	TEST_PERFORMANCE(definite_integral_rectangles(gauss, -3.0f, 3.0f, PRECISION), t2_rect,
 		"int(gauss) with rectangles approximation");
 
 	// Gaussian integration with rectangle approximation + SSE
-	TEST_PERFORMANCE(definite_integral_rectangles_sse(gauss, 0.0f, 20.0f, PRECISION), t2_rect_sse,
+	TEST_PERFORMANCE(definite_integral_rectangles_sse(gauss, -3.0f, 3.0f, PRECISION), t2_rect_sse,
 		"int(gauss) with rectangles approximation + SSE");
 
 	// Gaussian integration with Cavalieri-Simpson rule approximation
-	TEST_PERFORMANCE(definite_integral_cs(gauss, 0.0f, 20.0f, PRECISION), t2_cs,
+	TEST_PERFORMANCE(definite_integral_cs(gauss, -3.0f, 3.0f, PRECISION), t2_cs,
 		"int(gauss) with Cavalieri-Simpson rule approximation");
 
 	// Gaussian integration with Cavalieri-Simpson rule approximation + SSE
-	TEST_PERFORMANCE(definite_integral_cs_sse(gauss, 0.0f, 20.0f, PRECISION), t2_cs_sse,
+	TEST_PERFORMANCE(definite_integral_cs_sse(gauss, -3.0f, 3.0f, PRECISION), t2_cs_sse,
 		"int(gauss) with Cavalieri-Simpson rule approximation + SSE");
 
 	// Gaussian intgration with ad-hoc optimization + SSE
-	TEST_PERFORMANCE(gaussian_prob_sse(10.0f, 1.5f, 0.0f, 20.0f, PRECISION), t3_rect_sse,
+	TEST_PERFORMANCE(gaussian_prob_sse(0.0f, 1.0f, -3.0f, 3.0f, PRECISION), t3_rect_sse,
 		"int(gauss) with ad-hoc optimization (SSE)");
 
-	TEST_PERFORMANCE(gaussian_prob_avx2(10.0f, 1.5f, 0.0f, 20.0f, PRECISION), t3_rect_avx2,
+	TEST_PERFORMANCE(gaussian_prob_avx2(0.0f, 1.0f, -3.0f, 3.0f, PRECISION), t3_rect_avx2,
 		"int(gauss) with ad-hoc optimization (AVX2 with m256 registers)");
 }
 
